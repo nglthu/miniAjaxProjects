6f02a7dfdbe64d4f25fcd547491dd2ae6c45f57b
nglthu <nglthu@gmail.com>
2016-10-13 20:15:15 +1300

    StreetView Google to a background

--- js/script.js
+++ js/script.js
@@ -2,16 +2,21 @@
 function loadData() {
 
     var $body = $('body');
+	
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
 
+	var street = $('#street').val();
+	var city = $('#city').val();
+	var address = street + ',' + city;
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
     // load streetview
+$body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location='+address+'">')
 
     // YOUR CODE GOES HERE!
 
@@ -19,3 +24,4 @@ function loadData() {
 };
 
 $('#form-container').submit(loadData);
+
